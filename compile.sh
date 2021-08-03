064c7fc28a128500f477d8cd419a98dfa36f0938
Joonas Aunola <saltsa@gmail.com>
2021-08-03 19:05:52 +0300

    fix typos

--- compile.sh
+++ compile.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 set -e
 
-echo "building normal version to..."
+echo "building native covid-cert application.."
 go build -o covid-cert
-echo "building main.wasm..."
+echo "building webassembly main.wasm..."
 GOOS=js GOARCH=wasm go build -o main.wasm
 echo "building webserver..."
 go build -o http-server cmd/server/main.go
