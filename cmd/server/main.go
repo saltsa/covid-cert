30a4d0e8ae5297c50895c17690e7acb1dd835754
Joonas Aunola <saltsa@gmail.com>
2021-08-04 18:00:27 +0300

    moved web files to static

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -8,6 +8,10 @@ import (
 	"github.com/gorilla/handlers"
 )
 
+const (
+	dir = "./static"
+)
+
 func main() {
 	port := `:8080`
 	if envPort := os.Getenv("PORT"); envPort != "" {
@@ -15,7 +19,7 @@ func main() {
 	}
 	log.Printf("listening on %s", port)
 
-	http.Handle("/", http.FileServer(http.Dir(`.`)))
+	http.Handle("/", http.FileServer(http.Dir(dir)))
 	logHandler := handlers.LoggingHandler(os.Stdout, http.DefaultServeMux)
 
 	http.ListenAndServeTLS(port, "cert.pem", "key.pem", logHandler)
--- compile.sh
+++ compile.sh
@@ -4,6 +4,6 @@ set -e
 echo "building native covid-cert application.."
 go build -o covid-cert
 echo "building webassembly main.wasm..."
-GOOS=js GOARCH=wasm go build -o main.wasm
+GOOS=js GOARCH=wasm go build -o static/main.wasm
 echo "building webserver..."
 go build -o http-server cmd/server/main.go
